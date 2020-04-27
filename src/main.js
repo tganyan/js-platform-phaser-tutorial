be84bb0e7bf392f19f754dcf1730815a65e600a8
tganyan <tganyan@gmail.com>
2020-04-27 09:27:23 -0700

    initial commit - basic markup and starter js, add phaser js

---
+++ src/main.js
@@ -0,0 +1,15 @@
+const preload = () => {
+
+};
+
+const create = () => {
+
+};
+
+const update = () => {
+
+};
+
+const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
+	preload: preload, create: create, update: update
+});
