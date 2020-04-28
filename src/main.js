2cd0646d319565589e461fb6a032e2f12f1bc40a
tganyan <tganyan@gmail.com>
2020-04-28 09:01:56 -0700

    Update score, add win state, add player lives

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ let paddle;
 let bricks;
 let newBrick;
 let brickInfo;
+let scoreText;
+let score = 0;
+let lives = 3;
+let livesText;
+let lifeLostText;
+const textStyle = { font: '18px Arial', fill: '#0095dd' };
 
 // game functions
 const initBricks = () => {
@@ -37,6 +43,39 @@ const initBricks = () => {
 
 const ballHitBrick = (ball, brick) => {
 	brick.kill();
+	score += 10;
+	scoreText.setText(`Points: ${score}`);
+
+	let countAlive = 0;
+
+	for(i = 0; i < bricks.children.length; i++) {
+		if (bricks.children[i].alive === true) {
+			countAlive++;
+		}
+	}
+
+	if (countAlive === 0) {
+		alert('You win, congrats!');
+		location.reload();
+	}
+}
+
+const ballLeaveScreen = () => {
+	lives--;
+
+	if (lives) {
+		livesText.setText(`Lives: ${lives}`);
+		lifeLostText.visible = true;
+		ball.reset(game.world.width * 0.5, game.world.height - 25);
+		paddle.reset(game.world.width * 0.5, game.world.height - 5);
+		game.input.onDown.addOnce(() => {
+			lifeLostText.visible = false;
+			ball.body.velocity.set(150, -150);
+		}, this);
+	} else {
+		alert('You lost, game over!');
+		location.reload();
+	}
 }
 
 const preload = () => {
@@ -51,23 +90,32 @@ const preload = () => {
 
 const create = () => {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
+
 	ball = game.add.sprite(game.world.width * 0.5, game.world.height - 25, 'ball');
 	ball.anchor.set(0.5);
+
 	paddle = game.add.sprite(game.world.width * 0.5, game.world.height - 5, 'paddle');
 	paddle.anchor.set(0.5, 1);
+
 	game.physics.enable(ball, Phaser.Physics.ARCADE);
 	ball.body.collideWorldBounds = true;
 	ball.body.bounce.set(1);
 	ball.body.velocity.set(150, -150);
 	game.physics.arcade.checkCollision.down = false;
 	ball.checkWorldBounds = true;
-	ball.events.onOutOfBounds.add(() => {
-		alert('Game over!');
-		location.reload();
-	}, this);
+	ball.events.onOutOfBounds.add(ballLeaveScreen, this);
+
 	game.physics.enable(paddle, Phaser.Physics.ARCADE);
 	paddle.body.immovable = true;
 	initBricks();
+
+	scoreText = game.add.text(5, 5, 'Points: 0', textStyle);
+
+	livesText = game.add.text(game.world.width - 5, 5, `Lives: ${lives}`, textStyle);
+	livesText.acnhor.set(1, 0);
+	lifeLostText = game.add.text(game.world.width * 0.5, game.world.height * 0.5, 'Life lost, click to continue', textStyle);
+	lifeLostText.anchor.set(0.5);
+	lifeLostText.visible = false;
 };
 
 const update = () => {
