4cbf28c3af1be79f3d4e4a2caf51fb4158005cf2
tganyan <tganyan@gmail.com>
2020-04-28 08:27:43 -0700

    Load brick image, disperse brick field, add collision detection between ball and bricks

--- src/main.js
+++ src/main.js
@@ -1,7 +1,44 @@
 let ball;
 let paddle;
+let bricks;
+let newBrick;
+let brickInfo;
 
 // game functions
+const initBricks = () => {
+	brickInfo = {
+		width: 50,
+		height: 20,
+		count: {
+			row: 3,
+			 col: 7
+		},
+		offset: {
+			top: 50,
+			left: 60
+		},
+		padding: 10
+	};
+
+	bricks = game.add.group();
+
+	for (col = 0; col < brickInfo.count.col; col++) {
+		for (row = 0; row < brickInfo.count.row; row++) {
+			const brickX = (col * (brickInfo.width + brickInfo.padding)) + brickInfo.offset.top;
+			const brickY = (row * (brickInfo.height + brickInfo.padding)) + brickInfo.offset.left;
+			newBrick = game.add.sprite(brickX, brickY, 'brick');
+			game.physics.enable(newBrick, Phaser.Physics.ARCADE);
+			newBrick.body.immovable = true;
+			newBrick.anchor.set(0.5);
+			bricks.add(newBrick);
+		}
+	}
+}
+
+const ballHitBrick = (ball, brick) => {
+	brick.kill();
+}
+
 const preload = () => {
 	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 	game.scale.pageAlignHorizontally = true;
@@ -9,6 +46,7 @@ const preload = () => {
 	game.stage.backgroundColor = '#eee';
 	game.load.image('ball', '../img/ball.png');
 	game.load.image('paddle', '../img/paddle.png');
+	game.load.image('brick', '../img/brick.png');
 };
 
 const create = () => {
@@ -29,10 +67,12 @@ const create = () => {
 	}, this);
 	game.physics.enable(paddle, Phaser.Physics.ARCADE);
 	paddle.body.immovable = true;
+	initBricks();
 };
 
 const update = () => {
 	game.physics.arcade.collide(ball, paddle);
+	game.physics.arcade.collide(ball, bricks, ballHitBrick);
 	paddle.x = game.input.x || game.world.width * 0.5;
 };
 
